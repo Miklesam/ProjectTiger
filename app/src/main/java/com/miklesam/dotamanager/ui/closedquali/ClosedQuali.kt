de8f5a2a512e62285ad829ba7dd7b50dd21609a8
Miklesami <mikle.samarkin@gmail.com>
2020-05-13 23:31:40 +0300

    closed_layout

--- app/src/main/java/com/miklesam/dotamanager/ui/closedquali/ClosedQuali.kt
+++ app/src/main/java/com/miklesam/dotamanager/ui/closedquali/ClosedQuali.kt
@@ -1,6 +1,17 @@
 package com.miklesam.dotamanager.ui.closedquali
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.miklesam.dotamanager.R
+import kotlinx.android.synthetic.main.fragment_closed_quali.*
+import kotlinx.android.synthetic.main.group_stage_layout.view.*
 
-class ClosedQuali :Fragment(R.layout.fragment_closed_quali)
+class ClosedQuali :Fragment(R.layout.fragment_closed_quali){
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        groupLayoutA.Team1Name.text="Virtus Pro"
+        groupLayoutB.Team1Name.text="NaVi"
+    }
+}
